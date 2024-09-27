a857d6bdc37a6fced64fdcbc1464d2efcaf68e27
marcelohq <marcelomhq2@gmail.com>
2024-09-27 16:05:24 -0300

    starting to scrape

---
+++ scraping_project/main.py
@@ -0,0 +1,29 @@
+ #url https://quotes.toscrape.com/
+import requests
+from bs4 import BeautifulSoup
+from time import sleep
+
+all_quotes = []
+base_url = "https://quotes.toscrape.com"
+url = "/page/1/"
+
+while url:
+    res = requests.get(f"{base_url}{url}")
+    print(f"Now scrapping{base_url}{url} ...")
+    soup = BeautifulSoup(res.text, features="html.parser")
+    quotes = soup.find_all(class_="quote")
+    for quote in quotes:
+        all_quotes.append({
+                "quote":quote.find(class_ = "text").get_text(),
+                "author":quote.find(class_ = "author").get_text(),
+                "bio-link":quote.find("a")["href"]
+            })
+        
+    next_btn = soup.find(class_ = "next")
+    url = next_btn.find('a')['href'] if next_btn else None
+    print("waiting 3 seconds")
+    sleep(3)
+    
+
+print(all_quotes)
+    
